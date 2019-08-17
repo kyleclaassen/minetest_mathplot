01474c73badf9afa8a7b5a013be8ca1bc4fed0bd
Kyle Claassen <kyle.m.claassen@gmail.com>
2019-08-17 15:55:55 -0400

    Texture for origin node; require "destroyer" node to dig an origin node

--- nodes.lua
+++ nodes.lua
@@ -4,18 +4,35 @@ minetest.register_node("mathplot:origin", {
         description = "MathPlot Origin Node",
         groups = {cracky = 1},
         paramtype = "light",
-        light_source = 6,
-        tiles = {"^[colorize:#00FF00"},
+        light_source = 7,
+        --tiles = {"^[colorize:#00FF00"},
+        tiles = { "mathplot_origin_node_128x128.png" },
+        inventory_image = "mathplot_origin_node_128x128.png",
         on_construct = function(pos)
             --add to mod storage
-            local name = "Untitled Node"  --TODO: need some kind of uniqueness here... or change nodes to be stored by location instead of by name (probably better)
+            local name = "Untitled Node"
             mathplot.store_origin_location(name, pos)
         end,
-        on_rightclick = function(pos, node, player, itemstack, pointed_thing)
-            local context = {
-                node_pos = pos
-            }
-            mathplot.gui.invoke_screen("originmainmenu", player:get_player_name(), context)
+        is_ground_content = false,
+        on_blast = function(pos, intensity) end, --prevent explosions from destroying it
+        on_dig = function(pos, node, digger)
+            local wieldedItemName = digger:get_wielded_item():get_name()
+            if wieldedItemName == "mathplot:destroyer" then
+                minetest.node_dig(pos, node, digger)
+            end
+        end,
+        on_punch = function(pos, node, puncher, pointed_thing)
+            local wieldedItemName = puncher:get_wielded_item():get_name()
+            if wieldedItemName == "mathplot:destroyer" then
+                --Default "on_punch" behavior
+                minetest.node_punch(pos, node, puncher, pointed_thing)
+            else
+                --Show menu
+                local context = {
+                    node_pos = pos
+                }
+                mathplot.gui.invoke_screen("originmainmenu", puncher:get_player_name(), context)
+            end
         end,
         after_destruct = function(pos, oldnode)
             --remove from mod storage
@@ -23,3 +40,8 @@ minetest.register_node("mathplot:origin", {
         end
     })
 
+minetest.register_tool("mathplot:destroyer", {
+        description = "MathPlot Origin Node Destroyer",
+        groups = {},
+        inventory_image = "mathplot_tool_destroyer_128x128.png"
+    })
