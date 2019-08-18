854c5f17465dc5bc759791df1077790415ea5422
Kyle Claassen <kyle.m.claassen@gmail.com>
2019-08-18 02:50:42 -0400

    A few cosmetic tweaks to the validation errors screen

--- gui/screen_validation_errors.lua
+++ gui/screen_validation_errors.lua
@@ -5,9 +5,9 @@ mathplot.gui.screens["validation_errors"] = {
     end,
     get_formspec = function(playername, identifier, context)
         local errorsStr = minetest.formspec_escape(table.concat(context.errormsgs, "\n"))
-        local formspec = "size[5.25,6]"
+        local formspec = "size[5.5,6]"
         .. "label[0,0;Errors:]"
-        .. "textarea[0,0.5;5,5;;;" .. errorsStr .. "]"
+        .. "textarea[0.25,0.5;5.75,5.5;;;" .. errorsStr .. "]"
         .. "button_exit[0,5.5;2,1;btn_ok;OK]"
         return formspec
     end,
